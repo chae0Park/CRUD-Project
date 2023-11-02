1d7ad8b1d5f3541c860f79f6e3899ced373999d2
main <cypark022@gmail.com>
2023-11-02 23:00:45 +0900

    #4 Article, data.sql
    Atricle @OneToMany
    data.sql file added

--- CrudProject/src/main/java/CrudProject/domain/Article.java
+++ CrudProject/src/main/java/CrudProject/domain/Article.java
@@ -10,7 +10,9 @@ import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @ToString
@@ -32,6 +34,11 @@ public class Article {
     @Setter @Column(nullable = false, length = 10000) private String content;
 
     @Setter private String hashtag;
+
+    @ToString.Exclude
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
     @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;
     @CreatedBy @Column(nullable = false, length = 100) private String createdBy;
